ce471edd1c0ddfcbe9c8aecce3931ff13f0260a3
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-03-29 22:18:24 +0800

    tried to implement dymanic views

--- app.js
+++ app.js
@@ -2,8 +2,10 @@
 // https://www.npmjs.com/package/dotenv
 require("dotenv").config();
 
+
 // ℹ️ Connects to the database
 require("./db");
+
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const isLoggedOut = require("../middleware/isLoggedOut");
 const {isEmployer, isCompany, isLoggedIn} = require("../middleware/isLoggedIn");
 
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
