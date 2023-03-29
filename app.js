9f73d443ef21bb3c07a30aafe7e2d3bf288cd359
Hristos Pappasgit config --global user.name Hristos <hristos.pappas@icloud.com>
2023-03-30 00:09:53 +0800

    signup update

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const hbs = require("hbs");
 
 const app = express();
 
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
